ca53380388a486c83ee4fc63c4d839b04f83e073
boium <brandonoium@protonmail.com>
2019-03-29 13:38:18 -0500

    Added some basic bootstrapping

---
+++ PersonalSite/src/main/java/com/brandonoium/personalsite/bootstrap/DevBootstrap.java
@@ -0,0 +1,37 @@
+package com.brandonoium.personalsite.bootstrap;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import com.brandonoium.personalsite.model.User;
+import com.brandonoium.personalsite.repositories.CommentRepository;
+import com.brandonoium.personalsite.repositories.PostRepository;
+import com.brandonoium.personalsite.repositories.UserRepository;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+	private UserRepository userRepo;
+	private PostRepository postRepo;
+	private CommentRepository commentRepo;
+	
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
+		
+		System.out.println("Running bootstrap...");
+		
+		User tmp = new User(1, "Pyro");
+		userRepo.save(tmp);
+		
+		tmp = new User(2, "B-Rand");
+		userRepo.save(tmp);
+	}
+
+	public DevBootstrap(UserRepository userRepo, PostRepository postRepo, CommentRepository commentRepo) {
+		this.userRepo = userRepo;
+		this.postRepo = postRepo;
+		this.commentRepo = commentRepo;
+	}
+
+}
--- PersonalSite/src/main/java/com/brandonoium/personalsite/controllers/TutRestEndpoint.java
+++ PersonalSite/src/main/java/com/brandonoium/personalsite/controllers/TutRestEndpoint.java
@@ -1,14 +1,25 @@
 package com.brandonoium.personalsite.controllers;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brandonoium.personalsite.model.User;
+import com.brandonoium.personalsite.repositories.UserRepository;
+
 @RestController
 public class TutRestEndpoint {
+
+	private UserRepository userRepo;
+	
+	public TutRestEndpoint(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
 	
 	@CrossOrigin(origins="*")
 	@RequestMapping(value="/msg", method=RequestMethod.GET, produces="application/json")
@@ -16,9 +27,15 @@ public class TutRestEndpoint {
 	{
 		ArrayList<String> l = new ArrayList<String>();
 		
-		l.add("The message of the day is...");
-		l.add("Balla balla balla, don't forget to holla.");
+		Iterator<User> i = userRepo.findAll().iterator();
 		
+		while(i.hasNext())
+		{
+			l.add(i.next().getUsername());
+		}
+		
+		if(l.isEmpty())
+			l.add("No messages today!");
 		
 		return l;
 	}
